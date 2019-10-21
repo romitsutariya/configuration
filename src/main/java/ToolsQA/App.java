5e960d7bcddf2bdfc07977abd64db5c049356266
romitsutariya <romitsutariy@gmail.com>
2019-10-21 13:51:23 +0000

    Added Main method

--- src/main/java/ToolsQA/App.java
+++ src/main/java/ToolsQA/App.java
@@ -4,10 +4,30 @@ package ToolsQA;
  * Hello world!
  *
  */
+
+
+ import java.util.List;
+ 
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+
 public class App 
 {
+    public List<Employee> getEmployeeList(){
+ 
+        Session session = null;
+        session = HibernateUtil.getSession();
+        String queryStr = "select emp from Employee emp";
+        Query query = session.createQuery(queryStr);
+ 
+        return query.list();
+    }
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        EmployeesDao empDao = new EmployeesDao();
+        List<Employee> empList = empDao.getEmployeeList();
+        System.out.println("emp size: "+empList.size());
+        empList.stream().forEach(System.out::println);
     }
 }
